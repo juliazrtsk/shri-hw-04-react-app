17ba7b6d3fea47c8f522bf83b15807ae708af6bf
Julia Zaretskaya <julia.zrtsk@gmail.com>
2021-06-18 13:53:49 +0300

    Added linter and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true,
+    node: true,
+  },
+  extends: ['eslint:recommended', 'plugin:react/recommended', 'prettier'],
+  plugins: ['react', 'prettier'],
+  parserOptions: {
+    sourceType: 'module',
+    ecmaVersion: 12,
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  rules: {
+    'react/no-array-index-key': 'warn',
+  },
+};
