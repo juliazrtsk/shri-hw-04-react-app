f95bd762f262526497654e0b25a1825b90fd0de2
Julia Zaretskaya <julia.zrtsk@gmail.com>
2021-06-20 21:27:25 +0300

    Added config for the future localization

---
+++ src/localization/config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  layout_footer_support: 'Support',
+  layout_footer_learning: 'Learning',
+  layout_footer_ru: 'Русская версия',
+  layout_footer_author: 'Julia Zaretskaya',
+};
