7bb8c73f877f90f260c3cb5841b573a8347b5d43
Julia Zaretskaya <julia.zrtsk@gmail.com>
2021-06-21 01:09:45 +0300

    Added component for build meta info

---
+++ src/components/metaInfo/MetaInfo.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import cn from 'classnames';
+
+import './style.css';
+
+const MetaInfo = ({ className, children }) => {
+  return <div className={cn('meta', className)}>{children}</div>;
+};
+
+MetaInfo.propTypes = {
+  className: PropTypes.string,
+  children: PropTypes.node,
+};
+
+MetaInfo.defaultProps = {
+  className: '',
+};
+
+export default MetaInfo;
