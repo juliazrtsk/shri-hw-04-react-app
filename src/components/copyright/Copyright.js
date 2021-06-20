3a1f7c69fd7c2057c40b809e4959fcd3709d84cb
Julia Zaretskaya <julia.zrtsk@gmail.com>
2021-06-20 21:19:51 +0300

    Added copyright component

---
+++ src/components/copyright/Copyright.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import './style.css';
+
+const Copyright = ({ children }) => {
+  const currentYear = new Date().getFullYear();
+  return (
+    <span className="copyright">
+      © {currentYear} {children}
+    </span>
+  );
+};
+
+Copyright.propTypes = {
+  children: PropTypes.node,
+};
+
+Copyright.defaultProps = {
+  children: '',
+};
+
+export default Copyright;
