ea8e735ae69182b6762637c9e107aeed3309093b
Julia Zaretskaya <julia.zrtsk@gmail.com>
2021-06-21 16:02:29 +0300

    Added router and settings page route

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+
+import Layout from 'components/layout/Layout';
+import Settings from 'pages/settings/Settings';
 
 import './_variables.css';
 import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Layout>
+        <Switch>
+          <Route exact path="/settings" component={Settings} />
+          <Redirect to="/settings" />
+        </Switch>
+      </Layout>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
