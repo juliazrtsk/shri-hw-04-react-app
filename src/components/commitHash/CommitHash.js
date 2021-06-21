721903e814096ed4bd934b42f837b743f61d52a9
Julia Zaretskaya <julia.zrtsk@gmail.com>
2021-06-21 18:34:57 +0300

    Added component for commit hash

---
+++ src/components/commitHash/CommitHash.js
@@ -0,0 +1,38 @@
+import React, { useState, useCallback } from 'react';
+import PropTypes from 'prop-types';
+import cn from 'classnames';
+
+import './style.css';
+
+const CommitHash = ({ hash }) => {
+  const [expanded, setExpanded] = useState(false);
+
+  const onClick = useCallback(
+    (e) => {
+      e.stopPropagation();
+      setExpanded(!expanded);
+    },
+    [expanded]
+  );
+
+  return (
+    <div
+      className={cn('commit-hash', {
+        'commit-hash_expanded': expanded,
+      })}
+      onClick={onClick}
+    >
+      {hash}
+    </div>
+  );
+};
+
+CommitHash.propTypes = {
+  hash: PropTypes.string,
+};
+
+CommitHash.defaultProps = {
+  hash: '',
+};
+
+export default CommitHash;
