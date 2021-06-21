38368f42ef20b8d570395e1d25bbfba1dd6cb140
Julia Zaretskaya <julia.zrtsk@gmail.com>
2021-06-21 13:53:30 +0300

    Added component for input

---
+++ src/components/input/Input.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import cn from 'classnames';
+
+import './style.css';
+
+const Input = (props) => {
+  const { className, ...otherProps } = props;
+  return <input className={cn('input', className)} {...otherProps} />;
+};
+
+Input.propTypes = {
+  className: PropTypes.string,
+  onChange: PropTypes.func,
+};
+
+Input.defaultProps = {
+  className: '',
+  onChange: () => {},
+};
+
+export default Input;
