339aac469a94c54101d7e26865bb3a7951a0e442
Julia Zaretskaya <julia.zrtsk@gmail.com>
2021-06-28 14:31:03 +0300

    Added api instance with base REST requests

---
+++ src/api/Api.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+const API = process.env.REACT_APP_API_URL;
+
+class Api {
+  async get(path, params) {
+    let url = API + path;
+    const response = await axios.get(url, { params });
+    return response.data;
+  }
+
+  async post(path, params, data) {
+    let url = API + path;
+    const response = await axios.post(url, data, { params });
+    return response.data;
+  }
+}
+
+export default Api;
