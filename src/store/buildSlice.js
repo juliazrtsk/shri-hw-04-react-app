27aad12b8ff5e86b90ff5254d49c85200a4b95c1
Julia Zaretskaya <julia.zrtsk@gmail.com>
2021-06-30 18:29:37 +0300

    Added build slice

---
+++ src/store/buildSlice.js
@@ -0,0 +1,48 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+
+const initialState = {
+  details: null,
+  log: null,
+};
+
+export const getBuildDetails = createAsyncThunk(
+  'build/get-details',
+  async (buildId, { extra: { buildsService } }) => {
+    const { data } = await buildsService.getBuildDetails(buildId);
+    return data;
+  }
+);
+
+export const getBuildLogs = createAsyncThunk(
+  'build/get-logs',
+  async (buildId, { extra: { buildsService } }) => {
+    const { data } = await buildsService.getBuildLogs(buildId);
+    return data;
+  }
+);
+
+export const addBuildToQueue = createAsyncThunk(
+  'build/add-to-queue',
+  async (commitHash, { extra: { buildsService } }) => {
+    const { data } = await buildsService.addBuildToQueue(commitHash);
+    return data;
+  }
+);
+
+const buildSlice = createSlice({
+  name: 'build',
+  initialState: initialState,
+  extraReducers: (builder) => {
+    builder.addCase(getBuildDetails.fulfilled, (state, action) => {
+      return { ...state, details: action.payload };
+    });
+    builder.addCase(getBuildLogs.fulfilled, (state, action) => {
+      return { ...state, log: action.payload };
+    });
+  },
+});
+
+export const buildDetailsSelector = (state) => state.build.details;
+export const buildLogSelector = (state) => state.build.log;
+
+export const { reducer: buildReducer } = buildSlice;
--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 
+import { buildReducer } from './buildSlice';
 import { buildsReducer } from './buildsSlice';
 import { settingsReducer } from './settingsSlice';
 
 export const createStore = (options) =>
   configureStore({
     reducer: {
+      build: buildReducer,
       builds: buildsReducer,
       settings: settingsReducer,
     },
