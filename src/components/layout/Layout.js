b3161b01fa98829cc31d18b893224f0fc1ba70a1
Julia Zaretskaya <julia.zrtsk@gmail.com>
2021-06-30 18:06:41 +0300

    Made Run build button invisible if there are no settings

--- src/components/layout/Layout.js
+++ src/components/layout/Layout.js
@@ -1,6 +1,7 @@
 import React, { useCallback } from 'react';
 import PropTypes from 'prop-types';
 import { useHistory, useLocation } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
 
 import Header from 'components/header/Header';
 import Title from 'components/title/Title';
@@ -9,24 +10,47 @@ import Footer from 'components/footer/Footer';
 import l10n from 'l10n/config';
 import { matchLocationToPath, paths } from 'router';
 
+import { addBuildToQueue, buildDetailsSelector } from 'store/buildSlice';
+import { settingsSelector } from 'store/settingsSlice';
+
 import './style.css';
 
 const Layout = ({ children }) => {
   const history = useHistory();
   const location = useLocation();
+  const dispatch = useDispatch();
+
+  const build = useSelector(buildDetailsSelector);
+  const settings = useSelector(settingsSelector);
 
   const redirect = useCallback((path) => {
     history.push(path);
   }, []);
 
+  const rebuild = useCallback(async () => {
+    if (build) {
+      const { id } = await dispatch(addBuildToQueue(build.commitHash));
+      redirect(paths.build.replace(/:buildId/g, id));
+    }
+  }, [build]);
+
   const controls = {
     [paths.home]: [
-      { type: 'play', text: 'Run build', onClick: () => {} },
-      { type: 'settings', onClick: () => redirect(paths.settings) },
+      {
+        type: 'play',
+        text: 'Run build',
+        onClick: () => {},
+        visible: settings && settings.repoName,
+      },
+      {
+        type: 'settings',
+        onClick: () => redirect(paths.settings),
+        visible: true,
+      },
     ],
     [paths.settings]: [],
     [paths.build]: [
-      { type: 'restart', text: 'Rebuild', onClick: () => {} },
+      { type: 'restart', text: 'Rebuild', onClick: rebuild },
       { type: 'settings', onClick: () => redirect(paths.settings) },
     ],
   };
@@ -35,15 +59,18 @@ const Layout = ({ children }) => {
   const path = match ? match.path : paths.home;
 
   const renderControls = () =>
-    controls[path].map(({ type, text, onClick }) => (
-      <ActionButton
-        key={`layout_control_${type}`}
-        type={type}
-        onClick={onClick}
-      >
-        {text}
-      </ActionButton>
-    ));
+    controls[path].map(
+      ({ type, text, onClick, visible }) =>
+        visible && (
+          <ActionButton
+            key={`layout_control_${type}`}
+            type={type}
+            onClick={onClick}
+          >
+            {text}
+          </ActionButton>
+        )
+    );
 
   return (
     <div className="layout">
