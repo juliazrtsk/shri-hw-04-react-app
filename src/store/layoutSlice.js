aa8a3bf4a9c719a6399d0ce6f1c444b8bac125b0
Julia Zaretskaya <julia.zrtsk@gmail.com>
2021-07-01 09:26:55 +0300

    Added toggle modal window action

--- src/store/layoutSlice.js
+++ src/store/layoutSlice.js
@@ -2,9 +2,11 @@ import { createSlice, createAction } from '@reduxjs/toolkit';
 
 const initialState = {
   pending: false,
+  modalShown: false,
 };
 
 export const setPending = createAction('pending/set');
+export const toggleModal = createAction('modal/toggle');
 
 const layoutSlice = createSlice({
   name: 'layout',
@@ -13,9 +15,13 @@ const layoutSlice = createSlice({
     builder.addCase(setPending, (state, action) => {
       state.pending = action.payload;
     });
+    builder.addCase(toggleModal, (state) => {
+      state.modalShown = !state.modalShown;
+    });
   },
 });
 
 export const pendingSelector = (state) => state.layout.pending;
+export const modalShownSelector = (state) => state.layout.modalShown;
 
 export const { reducer: layoutReducer } = layoutSlice;
