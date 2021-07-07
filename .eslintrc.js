0c0c9526039477cab178716e7c6889684b916c6f
Julia Zaretskaya <julia.zrtsk@gmail.com>
2021-07-07 14:26:24 +0300

    Setup tests environment

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,9 +4,10 @@ module.exports = {
     commonjs: true,
     es2021: true,
     node: true,
+    'jest/globals': true,
   },
   extends: ['eslint:recommended', 'plugin:react/recommended', 'prettier'],
-  plugins: ['react', 'prettier'],
+  plugins: ['react', 'prettier', 'jest'],
   parserOptions: {
     sourceType: 'module',
     ecmaVersion: 12,
