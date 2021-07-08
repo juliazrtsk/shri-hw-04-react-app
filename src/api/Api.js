d95d2152894e0e89ab9135f6b18af914606767d4
Julia Zaretskaya <julia.zrtsk@gmail.com>
2021-07-08 15:52:22 +0300

    Added params from location.search to requests

--- src/api/Api.js
+++ src/api/Api.js
@@ -1,17 +1,22 @@
 import axios from 'axios';
+import queryString from 'query-string';
 
 const API = process.env.REACT_APP_API_URL;
 
 class Api {
   async get(path, params) {
     let url = API + path;
-    const response = await axios.get(url, { params });
+    const search = queryString.parse(location.search);
+    const response = await axios.get(url, { params: { ...search, ...params } });
     return response.data;
   }
 
   async post(path, data, params) {
     let url = API + path;
-    const response = await axios.post(url, data, { params });
+    const search = queryString.parse(location.search);
+    const response = await axios.post(url, data, {
+      params: { ...search, ...params },
+    });
     return response.data;
   }
 }
