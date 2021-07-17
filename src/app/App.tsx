efffa43f8fd502da312cc64db6262fcbd1ae3e86
Julia Zaretskaya <julia.zrtsk@gmail.com>
2021-07-17 20:23:45 +0300

    Added typescript to the project

---
+++ src/app/App.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Switch, Route, Redirect } from 'react-router-dom';
+
+import { routes, paths } from 'router';
+import Layout from 'components/layout/Layout';
+
+const App = () => (
+  <Layout>
+    <Switch>
+      {routes.map((route) => (
+        <Route path={route.path} key={route.path}>
+          <route.component loadData={route.loadData} />
+        </Route>
+      ))}
+      <Redirect to={paths.home} />
+    </Switch>
+  </Layout>
+);
+
+export default App;
